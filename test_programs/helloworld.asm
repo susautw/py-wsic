eb586fc93b5d173dddd13b799b66afdaffbca4b5
susautw <susautw@gmail.com>
2025-05-02 19:49:15 +0800

    Add address comments to instructions in helloworld.asm for clarity

--- test_programs/helloworld.asm
+++ test_programs/helloworld.asm
@@ -1,23 +1,23 @@
 HELLO  START  0000
-LOOP   LDX    LOC     # Set index with LOC
+LOOP   LDX    LOC     # 0x0000 Set index with LOC
 
-       LDA    ZERO    # Clear REG_A and load char
-       LDCH   STR,X
-       COMP   ZERO    # goto EXIT if loaded char is EOC
-       JEQ    EXIT    
+       LDA    ZERO    # 0x0003 Clear REG_A and load char
+       LDCH   STR,X   # 0x0006
+       COMP   ZERO    # 0x0009 goto EXIT if loaded char is EOC
+       JEQ    EXIT    # 0x000C
 
-       WD             # write the char
+       WD             # 0x000F write the char
 
-       LDA    LOC     # increase LOC by ONE
-       ADD    ONE
-       STA    LOC
-       J      LOOP    # goto LOOP
-EXIT   RSUB
+       LDA    LOC     # 0x0012 increase LOC by ONE
+       ADD    ONE     # 0x0015
+       STA    LOC     # 0x0018
+       J      LOOP    # 0x001B goto LOOP
+EXIT   RSUB           # 0x001E
 
-STR    BYTE   c'Hello World'
-       BYTE   0x0A
-EOC    BYTE   0x00
-LOC    WORD   0
-ONE    WORD   1
-ZERO   WORD   0
-END    
+STR    BYTE   c'Hello World'  # 0x0021
+       BYTE   0x0A            # 0x002C
+EOC    BYTE   0x00            # 0x002D
+LOC    WORD   0               # 0x002E
+ONE    WORD   1               # 0x0031
+ZERO   WORD   0               # 0x0034
+END                           # 0x0037
